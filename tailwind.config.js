8a9e2357f40eb8d904d286bd2e9680cf33aab3da
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-07-25 13:53:51 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and prettier-plugin-tailwindcss to package.json
    - Updated react-native-safe-area-context version to use caret (^) for flexibility
    - Added tailwindcss to package.json
    - Removed reset-project.js script as it is no longer needed
    - Created tailwind.config.js for Nativewind integration with custom theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}", // Keep this in case you still have an App file
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#000000",
+        secondary: "#ffffff",
+        tertiary: "#f5f5f5",
+        borders: "#E5E7EB"
+      }
+    },
+  },
+  plugins: [],
+}
