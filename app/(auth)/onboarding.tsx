eb2f981251831b0df9e4175a89552dfe81909b6e
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-07-27 23:11:09 +0100

    fix: add validation for work site selection in employee form

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,13 +1,43 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import React, { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
 const OnBoarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
-    <SafeAreaView className="flex flex-1 items-center justify-center">
-      <View>
-        <Text>OnBoarding</Text>
+    <SafeAreaView className="flex-1 bg-gradient-to-b from-primary to-secondary">
+      {/* Top bar */}
+      <View className="flex-row w-full justify-end p-4">
+        <TouchableOpacity
+          onPress={() => router.replace("/(auth)/login")}
+          className="flex-row items-center"
+        >
+          <Text className="text-white text-md font-bold">Skip</Text>
+          <MaterialCommunityIcons name="arrow-right" color="#fff" size={18} />
+        </TouchableOpacity>
       </View>
+
+      {/* Onboarding content */}
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="bg-white/50 w-[8px] h-[8px] rounded-full" />}
+        activeDot={<View className="bg-white w-[16px] h-[8px] rounded-full" />}
+      >
+        <View>
+          <Text>Slide 1</Text>
+        </View>
+        <View>
+          <Text>Slide 2</Text>
+        </View>
+        <View>
+          <Text>Slide 3</Text>
+        </View>
+      </Swiper>
     </SafeAreaView>
   );
 };
