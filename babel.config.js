9d61afeff54af09bb1494d5ca9bc2aab80e02e55
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-07-27 23:18:16 +0100

    fix: add @babel/plugin-proposal-export-namespace-from to dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
-      ],
-      plugins: ["react-native-worklets/plugin"],
-    };
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-worklets/plugin",
+    ],
   };
+};
