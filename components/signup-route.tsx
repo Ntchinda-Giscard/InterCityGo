336c363f641ad2dff80ae9d12e0042918d9b3514
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-08-12 15:15:59 +0100

    google and face book button with seperator

--- components/signup-route.tsx
+++ components/signup-route.tsx
@@ -63,12 +63,20 @@ const SignupRoute = () => {
         >
           <Text className="text-white text-base font-semibold"> Sign up </Text>
         </LinearGradient>
-        <View className="flex-row items-center w-full mt-5">
-          <View className="h-1 bg-white/50 rounded-full flex-1 mx-1" />
-          <Text className="text-white/50 font-normal">or continue with</Text>
-          <View className="h-1 bg-white/50 rounded-full flex-1 mx-1" />
-        </View>
       </TouchableOpacity>
+      <View className="flex-row items-center w-full mt-5 gap-4">
+        <View className="h-[1px] bg-white/50 rounded-full flex-1 mx-1" />
+        <Text className="text-white/50 font-normal">or continue with</Text>
+        <View className="h-[1px] bg-white/50 rounded-full flex-1 mx-1" />
+      </View>
+      <View className="flex-row items-center justify-center mt-4">
+        <TouchableOpacity className="bg-white/30 rounded-full px-4 py-2">
+          <Text className="text-white text-base font-normal">Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-white/30 rounded-full px-4 py-2 ml-4">
+          <Text className="text-white text-base font-normal">Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
