04510e8511cd492c37ec04dc70e89c6f6cc8d823
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-08-12 15:18:51 +0100

    google facebook images in constants

--- constants/images.ts
+++ constants/images.ts
@@ -1,6 +1,9 @@
+import facebook from "@/assets/images/facebook.png";
+import google from "@/assets/images/google.png";
 import logo from "@/assets/images/logo.png";
-
 
 export const images = {
   logo,
+  google,
+  facebook,
 };
