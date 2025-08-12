af9b5a04ad6142655f8d085bedc002499f6b5911
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-08-12 10:27:13 +0100

    top icon and text on login view

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,6 +1,7 @@
+import { images } from "@/constants/images";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useReducer } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const Welcome = () => {
   const [isToggled, toggle] = useReducer((s) => !s, false);
@@ -11,6 +12,14 @@ const Welcome = () => {
         colors={["#3B82F6", "#9333EA"]}
         style={styles.background}
       />
+      <View className=" w-full items-center justify-center pt-[20px] ">
+        <Image source={images.logo} className="w-[80px] h-[80px]" />
+        <Text className="text-white text-2xl font-bold"> InterCityGo </Text>
+        <Text className="text-white text-lg font-light mt-2">
+          Travel smart between cities
+        </Text>
+        <View className="bg-white/20 rounded-[24px] w-full mt-4 px-4 py-6"></View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -20,8 +29,6 @@ export default Welcome;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#9333EA",
   },
   background: {
