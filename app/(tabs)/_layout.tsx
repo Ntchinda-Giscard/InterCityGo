13519af6d07ca4fb183e49b14143d4b471e7fb4a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-08-14 16:05:54 +0100

    google facebook images in constants

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,14 +7,6 @@ const LayOut = () => {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          height: 60,
-          backgroundColor: "#fff",
-        },
       }}
     >
       <Tabs.Screen
